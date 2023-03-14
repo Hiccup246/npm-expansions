7d565c43b6cb9946d81b3fdcefa98a37cd3d1929
James Watt <james.watt529@outlook.com>
2023-03-14 14:06:55 +0000

    Update lib.rs file with comments outlining implementation details and development approach

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+// Single GET route which:
+// - Returns a JSON response containing a random npm expansion if content-type header is set to JSON
+// - Returns HTML if the content-type is set to HTML
+// - Serves appropriate static files for SEO, robots and favicons
+// - All other routes return an appropraite error response
+
+// System should:
+// - Handle incoming requests in an optimised way e.g. threads or asynchronous events
+
+// Implementation plan
+// - Tests first for functionality of JSON GET route
+// - Start in main and split to library files when needed
+// - When JSON route is completed move to HTML
+// - Start single threaded then move to multi threaded (tracer rounds approach)
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
