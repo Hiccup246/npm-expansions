7417fc7f23a11fa701e2b7a239ffdb83f93611ce
James Watt <james.watt529@outlook.com>
2023-03-17 01:02:07 +0000

    Update lib todo comments

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,16 @@
 // - Handle incoming requests in an optimised way e.g. threads or asynchronous events
 
 // Implementation plan
-// - Tests first for functionality of JSON GET route
-// - Start in main and split to library files when needed
-// - When JSON route is completed move to HTML
-// - Start single threaded then move to multi threaded (tracer rounds approach)
+// - Implement Accept header pasrsing
+// - Figure out error behaviour (rendering 500 page)
+// - Render static files at static routes
+// - Figure out how to structure tests in a more readable manner
+// - Refactor all code and tests into a more organised structure
+//   following best practices
+// - Implement NPM expansions home page in figma
+// - Implement 404 and 500 pages
+// - Implement CICD with github actions
+// - Implement husky with testing and formatting
 // - 500 internal server error for any errors
 // - Perhaps move each route code into a struct called "controller"?
 
