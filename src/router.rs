da376258bc0ef31f313b82f46e88c4434735344b
James Watt <james.watt529@outlook.com>
2023-03-21 09:28:01 +0000

    Add documentation comments for route_request function

--- src/router.rs
+++ src/router.rs
@@ -14,6 +14,41 @@ impl Router<'_> {
         Router { routes_config }
     }
 
+    /// Returns a byte response to an incoming request
+    ///
+    /// # Arguments
+    ///
+    /// * `request` - A Request struct
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let actual_route: fn(&Request) -> Vec<u8> = |_| "actual_route".as_bytes().to_vec();
+    /// let route_config: HashMap<&str, fn(&Request) -> Vec<u8>> =
+    ///     HashMap::from([("GET / HTTP/1.1", actual_route)]);
+    /// let router = Router::new(route_config);
+    /// let request = Request {
+    ///     status_line: "GET / HTTP/1.1".to_string(),
+    ///     headers: HashMap::new(),
+    /// };
+    /// let response = router.route_request(request);
+    /// assert_eq!(response.unwrap(), "actual_route".as_bytes().to_vec())
+    /// ```
+    ///
+    /// # Failures
+    ///
+    /// The function fails if an unrecognized route is given and no 404 route exists
+    ///
+    /// ```rust,should_error
+    /// // fails if no 404 route exists and a unknown route is given
+    /// let router = Router::new(HashMap::new());
+    /// let request = Request {
+    ///     status_line: "GET / HTTP/1.1".to_string(),
+    ///     headers: HashMap::new(),
+    /// };
+    /// let response = router.route_request(request);
+    /// assert!(response.is_err())
+    /// ```
     pub fn route_request(&self, request: Request) -> Result<Vec<u8>, HandleRouteError> {
         let status_line = request.status_line();
         let controller_function = self.routes_config.get(status_line);
