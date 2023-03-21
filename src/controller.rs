a2ccb28cac9c35d2dfbea9b6933cde01ba3afc3e
James Watt <james.watt529@outlook.com>
2023-03-21 20:56:18 +0000

    Refactor controller to use match statements

--- src/controller.rs
+++ src/controller.rs
@@ -2,163 +2,159 @@ use crate::accept_header_handler;
 use crate::NpmExpansions;
 use crate::Request;
 use std::fs;
+use crate::npm_expansion_error::NpmErrorKind;
 
 pub struct Controller {}
 
+fn ok(contents: String) -> Vec<u8> {
+    let length = contents.len();
+    format!("HTTP/1.1 200 OK\r\nContent-Length: {length}\r\n\r\n{contents}").as_bytes().to_vec()
+}
+
+fn not_acceptable(contents: String) -> Vec<u8> {
+    let length = contents.len();
+    format!("HTTP/1.1 406 Not Acceptable\r\nContent-Length: {length}\r\nContent-Type: application/json\r\n\r\n{contents}").as_bytes().to_vec()
+}
+
+fn not_found_route(contents: String) -> Vec<u8> {
+    let length = contents.len();
+    format!("HTTP/1.1 404 NOT FOUND\r\nContent-Length: {length}\r\n\r\n{contents}").as_bytes().to_vec()
+}
+
+fn internal(contents: String) -> Vec<u8> {
+    let length = contents.len();
+    format!("HTTP/1.1 500 INTERNAL SERVER ERROR\r\nContent-Length: {length}\r\n\r\n{contents}").as_bytes().to_vec()
+}
+
+fn bad_req(contents: String) -> Vec<u8> {
+    let length = contents.len();
+    format!("HTTP/1.1 400 BAD REQUEST\r\nContent-Length: {length}\r\n\r\n{contents}").as_bytes().to_vec()
+}
+
 impl Controller {
     pub fn index(request: &Request) -> Vec<u8> {
-        let response;
+        let mut response:Vec<u8> = Vec::new();
 
-        // If request accepts application/json then we are good to go
         let best = accept_header_handler::best_match(
             Vec::from(["text/html", "text/css", "text/javascript"]),
             request.headers().get("Accept").unwrap(),
-        )
-        .unwrap();
+        );
+        
+        if let Ok(best_match) = best {
+            response = match best_match.as_str() {
+                "text/html" => ok(fs::read_to_string("pages/npm_expansions/npm_expansions.html").unwrap()),
+                "text/css" => ok(fs::read_to_string("pages/npm_expansions/npm_expansions.css").unwrap()),
+                "text/javascript" => ok(fs::read_to_string("pages/npm_expansions/npm_expansions.js").unwrap()),
+                _ => not_acceptable(format!("Please accept text/html, text/css or text/javascript")),
+            }
+        } else if let Err(err) = best {
+            response = match err.kind() {
+                NpmErrorKind::SupportedMimeTypeError => Controller::internal_server_error(request),
+                NpmErrorKind::InvalidHeader => Controller::client_error(request),
+                _ => panic!("Unknown error")
+            };
+        };
 
-        if best == "text/html" {
-            let status_line = "HTTP/1.1 200 OK";
-            let contents = fs::read_to_string("pages/npm_expansions/npm_expansions.html").unwrap();
-            let length = contents.len();
-
-            response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        } else if best == "text/css" {
-            let status_line = "HTTP/1.1 200 OK";
-            let contents = fs::read_to_string("pages/npm_expansions/npm_expansions.css").unwrap();
-            let length = contents.len();
-
-            response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        } else if best == "text/javascript" {
-            let status_line = "HTTP/1.1 200 OK";
-            let contents = fs::read_to_string("pages/npm_expansions/npm_expansions.js").unwrap();
-            let length = contents.len();
-
-            response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        } else {
-            let status_line = "HTTP/1.1 406 Not Acceptable";
-            let contents = format!("Please accept text/html");
-            let length = contents.len();
-
-            response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        }
-
-        response.as_bytes().to_vec()
+        response
     }
 
-    pub fn random<'a>(request: &Request) -> Vec<u8> {
-        let response: String;
+    pub fn random(request: &Request) -> Vec<u8> {
+        let mut response:Vec<u8> = Vec::new();
+
         let best = accept_header_handler::best_match(
             Vec::from(["application/json"]),
             request.headers().get("Accept").unwrap(),
-        )
-        .unwrap();
+        );
 
-        if best == "application/json" {
-            let status_line = "HTTP/1.1 200 OK";
-            let expansion = NpmExpansions::random_expansion();
-            let contents = format!("{{\"npm-expansion\": \"{expansion}\"}}");
-            let length = contents.len();
-            let content_type = "application/json";
+        if let Ok(best_match) = best {
+            response = match best_match.as_str() {
+                "application/json" => ok(format!("{{\"npm-expansion\": \"{}\"}}", NpmExpansions::random_expansion())),
+                _ => not_acceptable(format!("Please accept application/json")),
+            }
+        } else if let Err(err) = best {
+            response = match err.kind() {
+                NpmErrorKind::SupportedMimeTypeError => Controller::internal_server_error(request),
+                NpmErrorKind::InvalidHeader => Controller::client_error(request),
+                _ => panic!("Unknown error")
+            };
+        };
 
-            response = format!("{status_line}\r\nContent-Length: {length}\r\nContent-Type: {content_type}\r\n\r\n{contents}\r\n")
-        } else {
-            let status_line = "HTTP/1.1 406 Not Acceptable";
-            let contents = format!("Please accept application/json");
-            let length = contents.len();
-
-            response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        }
-
-        response.as_bytes().to_vec()
+        response
     }
 
     pub fn not_found(request: &Request) -> Vec<u8> {
-        let mut status_line = "HTTP/1.1 404 NOT FOUND";
-        let response;
+        let mut response:Vec<u8> = Vec::new();
 
-        // If request accepts application/json then we are good to go
         let best = accept_header_handler::best_match(
             Vec::from(["application/json", "text/html"]),
             request.headers().get("Accept").unwrap(),
-        )
-        .unwrap();
+        );
 
-        if best == "application/json" {
-            response = format!("{status_line}\r\n\r\n");
-        } else if best == "text/html" {
-            let contents = fs::read_to_string("pages/not_found/not_found.html").unwrap();
-            let length = contents.len();
+        if let Ok(best_match) = best {
+            response = match best_match.as_str() {
+                "application/json" => not_found_route(format!("NOT FOUND")),
+                "text/html" => not_found_route(fs::read_to_string("pages/not_found/not_found.html").unwrap()),
+                _ => not_acceptable(format!("Please accept application/json or text/html")),
+            }
+        } else if let Err(err) = best {
+            response = match err.kind() {
+                NpmErrorKind::SupportedMimeTypeError => Controller::internal_server_error(request),
+                NpmErrorKind::InvalidHeader => Controller::client_error(request),
+                _ => panic!("Unknown error")
+            };
+        };
 
-            response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        } else {
-            status_line = "HTTP/1.1 406 Not Acceptable";
-            let contents = format!("Please accept application/json or text/html");
-            let length = contents.len();
-
-            response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        }
-
-        response.as_bytes().to_vec()
+        response
     }
 
     pub fn internal_server_error(request: &Request) -> Vec<u8> {
-        let mut status_line = "HTTP/1.1 500 INTERNAL SERVER ERROR";
-        let response;
+        let mut response:Vec<u8> = Vec::new();
 
-        // If request accepts application/json then we are good to go
         let best = accept_header_handler::best_match(
             Vec::from(["application/json", "text/html"]),
             request.headers().get("Accept").unwrap(),
-        )
-        .unwrap();
+        );
 
-        if best == "application/json" {
-            response = format!("{status_line}\r\n\r\n");
-        } else if best == "text/html" {
-            let contents =
-                fs::read_to_string("pages/internal_server_error/internal_server_error.html")
-                    .unwrap();
-            let length = contents.len();
+        if let Ok(best_match) = best {
+            response = match best_match.as_str() {
+                "application/json" => internal(format!("INTERNAL SERVER ERROR")),
+                "text/html" => internal(fs::read_to_string("pages/internal_server_error/internal_server_error.html").unwrap()),
+                _ => not_acceptable(format!("Please accept application/json or text/html")),
+            }
+        } else if let Err(err) = best {
+            response = match err.kind() {
+                NpmErrorKind::SupportedMimeTypeError => Controller::internal_server_error(request),
+                NpmErrorKind::InvalidHeader => Controller::client_error(request),
+                _ => panic!("Unknown error")
+            };
+        };
 
-            response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        } else {
-            status_line = "HTTP/1.1 406 Not Acceptable";
-            let contents = format!("Please accept application/json or text/html");
-            let length = contents.len();
-
-            response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        }
-
-        response.as_bytes().to_vec()
+        response
     }
 
     pub fn client_error(request: &Request) -> Vec<u8> {
-        let mut status_line = "HTTP/1.1 400 BAD REQUEST";
-        let response;
+        let mut response:Vec<u8> = Vec::new();
 
-        // If request accepts application/json then we are good to go
         let best = accept_header_handler::best_match(
             Vec::from(["application/json", "text/html"]),
             request.headers().get("Accept").unwrap(),
-        )
-        .unwrap();
+        );
 
-        if best == "application/json" {
-            response = format!("{status_line}\r\n\r\n");
-        } else if best == "text/html" {
-            let contents = fs::read_to_string("pages/client_error/client_error.html").unwrap();
-            let length = contents.len();
+        if let Ok(best_match) = best {
+            response = match best_match.as_str() {
+                "application/json" => bad_req(format!("BAD REQUEST")),
+                "text/html" => bad_req(fs::read_to_string("pages/client_error/client_error.html").unwrap()),
+                _ => not_acceptable(format!("Please accept application/json or text/html")),
+            }
+        } else if let Err(err) = best {
+            response = match err.kind() {
+                NpmErrorKind::SupportedMimeTypeError => Controller::internal_server_error(request),
+                NpmErrorKind::InvalidHeader => Controller::client_error(request),
+                _ => panic!("Unknown error")
+            };
+        };
 
-            response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        } else {
-            status_line = "HTTP/1.1 406 Not Acceptable";
-            let contents = format!("Please accept application/json or text/html");
-            let length = contents.len();
-
-            response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        }
-
-        response.as_bytes().to_vec()
+        response
     }
 
     pub fn static_file(request: &Request) -> Vec<u8> {
