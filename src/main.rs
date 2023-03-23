51dff462e76b7356db92610bf08e9f34caf50f46
James Watt <james.watt529@outlook.com>
2023-03-23 11:11:07 +0000

    Add tests for respond_to_request and success tests for connection_handler

--- src/main.rs
+++ src/main.rs
@@ -25,14 +25,14 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let mut stream = stream.unwrap();
 
-        connection_handler(stream, &router);
+        handle_connection(&mut stream, &router);
     }
 }
 
-fn connection_handler(mut stream: TcpStream, router: &router::Router) {
-    let response = respond_to_request(&mut stream, router);
+fn handle_connection(stream: &mut (impl Read + Write), router: &router::Router) {
+    let response = respond_to_request(stream, router);
 
     if let Err(res) = response {
         let error_request = Request::new(
@@ -55,7 +55,7 @@ fn connection_handler(mut stream: TcpStream, router: &router::Router) {
         };
 
         if let Ok(error_res) = error_response {
-            stream.write_all(error_res.as_slice()).unwrap()
+            return stream.write_all(error_res.as_slice()).unwrap();
         }
 
         panic!("Unconditional server failure. This is a server bug!")
@@ -76,8 +76,158 @@ fn respond_to_request(
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    extern crate tempdir;
 
+    use super::*;
+    use crate::mock_tcp_stream::MockTcpStream;
+
+    mod respond_to_request {
+        use super::*;
+
+        #[test]
+        fn valid_http_stream() {
+            let input_bytes = b"GET / HTTP/1.1\r\nAccept: text/html\r\n\r\n";
+            let mut contents = vec![0u8; 1024];
+
+            contents[..input_bytes.len()].clone_from_slice(input_bytes);
+
+            let mut stream = MockTcpStream {
+                read_data: contents,
+                write_data: Vec::new(),
+            };
+
+            let router = router::Router::new(HashMap::from([(
+                "GET / HTTP/1.1".to_string(),
+                Controller::index as fn(&Request) -> Result<Vec<u8>, NpmExpansionsError>,
+            )]));
+
+            let response = respond_to_request(&mut stream, &router);
+
+            assert!(response.is_ok());
+        }
+
+        #[test]
+        fn no_not_found_route() {
+            let input_bytes = b"GET /non-existant/route HTTP/1.1\r\nAccept: text/html\r\n\r\n";
+            let mut contents = vec![0u8; 1024];
+
+            contents[..input_bytes.len()].clone_from_slice(input_bytes);
+
+            let mut stream = MockTcpStream {
+                read_data: contents,
+                write_data: Vec::new(),
+            };
+
+            let router = router::Router::new(HashMap::from([(
+                "GET / HTTP/1.1".to_string(),
+                Controller::index as fn(&Request) -> Result<Vec<u8>, NpmExpansionsError>,
+            )]));
+
+            let response = respond_to_request(&mut stream, &router);
+
+            assert!(response.is_err());
+        }
+
+        #[test]
+        fn invalid_request() {
+            let input_bytes = b"";
+            let mut contents = vec![0u8; 1024];
+
+            contents[..input_bytes.len()].clone_from_slice(input_bytes);
+
+            let mut stream = MockTcpStream {
+                read_data: contents,
+                write_data: Vec::new(),
+            };
+
+            let router = router::Router::new(HashMap::from([(
+                "GET / HTTP/1.1".to_string(),
+                Controller::index as fn(&Request) -> Result<Vec<u8>, NpmExpansionsError>,
+            )]));
+
+            let response = respond_to_request(&mut stream, &router);
+
+            assert!(response.is_err());
+        }
+    }
+
+    mod connection_handler {
+        use super::*;
+
+        #[test]
+        fn valid_http_stream() {
+            let input_bytes = b"GET / HTTP/1.1\r\nAccept: text/html\r\n\r\n";
+            let mut contents = vec![0u8; 1024];
+
+            contents[..input_bytes.len()].clone_from_slice(input_bytes);
+
+            let mut stream = MockTcpStream {
+                read_data: contents,
+                write_data: Vec::new(),
+            };
+
+            let router = router::Router::new(HashMap::from([(
+                "GET / HTTP/1.1".to_string(),
+                Controller::index as fn(&Request) -> Result<Vec<u8>, NpmExpansionsError>,
+            )]));
+
+            let response = handle_connection(&mut stream, &router);
+
+            assert_eq!(response, ());
+        }
+
+        #[test]
+        fn invalid_http_stream() {
+            let input_bytes = b"";
+            let mut contents = vec![0u8; 1024];
+
+            contents[..input_bytes.len()].clone_from_slice(input_bytes);
+
+            let mut stream = MockTcpStream {
+                read_data: contents,
+                write_data: Vec::new(),
+            };
+
+            let router = router::Router::new(HashMap::from([(
+                "GET / HTTP/1.1".to_string(),
+                Controller::index as fn(&Request) -> Result<Vec<u8>, NpmExpansionsError>,
+            )]));
+
+            let response = handle_connection(&mut stream, &router);
+
+            assert_eq!(response, ());
+        }
+
+        // #[test]
+        // fn invalid_http_stream_with_error_response() {
+        //     let input_bytes = b"";
+        //     let mut contents = vec![0u8; 1024];
+
+        //     contents[..input_bytes.len()].clone_from_slice(input_bytes);
+
+        //     let mut stream = MockTcpStream {
+        //         read_data: contents,
+        //         write_data: Vec::new(),
+        //     };
+
+        //     fn panic_controller_method(request: &Request) -> Result<Vec<u8>, NpmExpansionsError>{
+
+        //     }
+
+        //     let router = router::Router::new(
+        //         HashMap::from([
+        //             (
+        //                 "GET / HTTP/1.1".to_string(),
+        //                 Controller::index as fn(&Request) -> Result<Vec<u8>, NpmExpansionsError>,
+        //             )
+        //         ])
+        //     );
+
+        //     let response = handle_connection(&mut stream, &router);
+
+        //     assert_eq!(response, ());
+        // }
+    }
     // fn setup_server_and_connection() -> (TcpListener, TcpStream) {
     //     (
     //         TcpListener::bind("127.0.0.1:7878").unwrap(),
