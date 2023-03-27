5816215da1220e4f5e07104a24f5b4e81037abb9
James Watt <james.watt529@outlook.com>
2023-03-27 17:42:16 +0100

    Add controller all function to routes_config

--- npm-expansions/src/routes_config.rs
+++ npm-expansions/src/routes_config.rs
@@ -34,6 +34,10 @@ pub fn route_config(static_directory: &str) -> Route {
             Controller::random as fn(&Request) -> Result<Vec<u8>, NpmExpansionsError>,
         ),
         (
+            "GET /all HTTP/1.1".to_string(),
+            Controller::all as fn(&Request) -> Result<Vec<u8>, NpmExpansionsError>,
+        ),
+        (
             "404".to_string(),
             Controller::not_found as fn(&Request) -> Result<Vec<u8>, NpmExpansionsError>,
         ),
