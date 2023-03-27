00654b2ed30e071a367f40aa6ae14cf274a5cf37
James Watt <james.watt529@outlook.com>
2023-03-27 11:41:27 +0100

    Update build.rs file comments

--- npm-expansions/build.rs
+++ npm-expansions/build.rs
@@ -6,16 +6,19 @@ fn main() {
 
     File::create("target/hello-builder.txt").unwrap();
     // We have two key choices
-    // 1. Macros which imbed the code and uglifies it at compile time
+    // 1. Macros which imbed the code
     //   - Hashmap access i.e. Assets::get("pages/index.html");
     //   - Const access i.e. INDEX_PAGE
-    // 2. Simply imbed assets when we need them in each controller and uglify at runtime
+    // 2. Simply imbed assets when we need them in each controller
     //   - include_bytes!("pages/index.html");
     // 3. Build time commands
     //   - Cargo builds and uglifies at run time and stores files in target
     //
     // Benefits and Drawbacks
-    // 1. Binary works standalone without any extra files
-    // 2. Extra run time compilation
+    // 1. Access is standadised (uglification at run or compile time)
+    // 2. Access on a per file basis (uglification at run or compile time)
     // 3. Binary cannot work standalone
+
+    // 1 and 2 are identical in all aspects except access
+    // Either way we will have to uglify at build time so we should just do that
 }
