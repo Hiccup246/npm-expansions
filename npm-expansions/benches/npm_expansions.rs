30faf38701796ace859d04a2240270f1fd937fd6
James Watt <james.watt529@outlook.com>
2023-03-29 14:47:34 +0100

    Add npm-expansions benchmark tests

---
+++ npm-expansions/benches/npm_expansions.rs
@@ -0,0 +1,20 @@
+#![feature(test)]
+
+extern crate test;
+use npm_expansions::npm_expansions::NpmExpansions;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn generating_random_expansions(b: &mut Bencher) {
+        b.iter(|| NpmExpansions::random_expansion());
+    }
+
+    #[bench]
+    fn generating_all_expansions(b: &mut Bencher) {
+        b.iter(|| NpmExpansions::expansions());
+    }
+}
