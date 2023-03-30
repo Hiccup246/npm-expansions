511d747d82e5afc65067628fbf0e24995c095f06
James Watt <james.watt529@outlook.com>
2023-03-30 16:56:59 +0100

    Add documentation comments for enums and structs in npm_expansion_error.rs

--- npm-expansions/src/npm_expansion_error.rs
+++ npm-expansions/src/npm_expansion_error.rs
@@ -1,8 +1,13 @@
+/// Error type of the server
+///
+/// In order for the server to process different types of errors and
+/// return appropriate responses i.e. 400, 500 the error has a kind attribute
 #[derive(Debug)]
 pub struct NpmExpansionsError {
     kind: NpmErrorKind,
 }
 
+/// Enum for the different kinds of errors the server can produce
 #[derive(Debug)]
 pub enum NpmErrorKind {
     InvalidHeader,
