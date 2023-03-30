2044bea44f78d9017fbca0e33edb5e8694f8e964
James Watt <james.watt529@outlook.com>
2023-03-30 15:13:16 +0100

    Update npm_expansions page to have interactive search components

--- nginx-reverse-proxy/pages/npm_expansions/npm_expansions.js
+++ nginx-reverse-proxy/pages/npm_expansions/npm_expansions.js
@@ -5,4 +5,51 @@ async function generateRandomExpansion() {
     document.querySelector(".npm-expansion").innerHTML = randomExpansionJSONResponse["npmExpansion"];
 }
 
+async function loadExpansionIntoTextArea() {
+    const randomExpansionResponse = await fetch("/api/random");
+    const randomExpansionJSONResponse = await randomExpansionResponse.json();
+
+    document.querySelector(".results-expansions-list").innerHTML = randomExpansionJSONResponse;
+}
+
+async function searchExpansions(query) {
+    const searchExpansionResponse = await fetch(`/api/search?query=${query.target.value}`);
+    const searchExpansionJSONResponse = await searchExpansionResponse.json();
+    
+    const singleString = searchExpansionJSONResponse.reduce("", (acc, expansion) => acc + (expansion + "\n"));
+    document.querySelector(".results-expansions-list").innerHTML = singleString;
+}
+
+async function loadAllExpansions() {
+    const allExpansionResponse = await fetch("/api/all");
+    const allExpansionJSONResponse = await allExpansionResponse.json();
+
+    const singleString = allExpansionJSONResponse.reduce("", (acc, expansion) => acc + (expansion + "\n"));
+    document.querySelector(".results-expansions-list").innerHTML = singleString;
+}
+
+function debounce (context, func, delay) {
+    let timeout;
+
+    return (...arguments) => {
+        if (timeout) {
+        clearTimeout(timeout);
+        }
+
+        timeout = setTimeout(() => {
+        func.apply(context, arguments);
+        }, delay);
+    };
+};
+
+const debouncedSearchState = debounce(
+    this,
+    searchTerm => searchExpansions(searchTerm), // an expensive function
+    100
+);
+
 generateRandomExpansion();
+loadExpansionIntoTextArea();
+
+const input = document.querySelector(".results-expansions-list");
+input.addEventListener("input", debouncedSearchState);
