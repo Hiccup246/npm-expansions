45e297c7e4cac29ddb712c582164ebee75834d83
James Watt <james.watt529@outlook.com>
2023-04-03 20:52:25 +0100

    Rename benches tests

---
+++ npm-expansions/benches/expansions_model.rs
@@ -0,0 +1,31 @@
+#![feature(test)]
+
+extern crate test;
+use npm_expansions::expansions_model::ExpansionsModel;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use npm_expansions::expansions_model::ExpansionsAccess;
+    use test::Bencher;
+
+    #[bench]
+    fn random_expansions(b: &mut Bencher) {
+        let expansions_generator = ExpansionsModel::build("rsc/expansions.txt");
+        b.iter(|| expansions_generator.random_expansion());
+    }
+
+    #[bench]
+    fn all_expansions(b: &mut Bencher) {
+        let expansions_generator = ExpansionsModel::build("rsc/expansions.txt");
+
+        b.iter(|| expansions_generator.all());
+    }
+
+    #[bench]
+    fn search_expansions(b: &mut Bencher) {
+        let expansions_generator = ExpansionsModel::build("rsc/expansions.txt");
+
+        b.iter(|| expansions_generator.search("Nacho Person Manager"));
+    }
+}
