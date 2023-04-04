bfeb43b70be40d3b729eb227284028b3e0a6dc16
James Watt <james.watt529@outlook.com>
2023-04-04 13:22:20 +0100

    Enable rust doc lints, add documentation comments to nginx-reverse-proxy lib.rs

--- nginx-reverse-proxy/src/lib.rs
+++ nginx-reverse-proxy/src/lib.rs
@@ -1,1 +1,10 @@
+//! # NPM Expansions Static
+//!
+//! A library allowing for static files and directories to be minified
+
+#![feature(rustdoc_missing_doc_code_examples)]
+#![warn(missing_docs)]
+#![warn(rustdoc::missing_doc_code_examples)]
+
+/// A series of utility functions that allow files and directories to be minifed
 pub mod asset_minifier;
