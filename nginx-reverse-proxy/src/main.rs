23edd04e49b755d01c3777b554cb65651acddfd9
James Watt <james.watt529@outlook.com>
2023-04-04 14:54:32 +0100

    Format main.rs with cargo fmt

--- nginx-reverse-proxy/src/main.rs
+++ nginx-reverse-proxy/src/main.rs
@@ -2,8 +2,6 @@ use asset_minifier::minify;
 use std::path::PathBuf;
 
 fn main() {
-    minify::minify_drectory(PathBuf::from("pages"), PathBuf::from("minified_pages"))
-        .unwrap();
-    minify::minify_drectory(PathBuf::from("static"), PathBuf::from("minified_static"))
-        .unwrap();
+    minify::minify_drectory(PathBuf::from("pages"), PathBuf::from("minified_pages")).unwrap();
+    minify::minify_drectory(PathBuf::from("static"), PathBuf::from("minified_static")).unwrap();
 }
