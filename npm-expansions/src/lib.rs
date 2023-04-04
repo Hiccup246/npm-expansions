9b8cca7f0729d3677fa2114c7857ac442e9fc945
James Watt <james.watt529@outlook.com>
2023-04-04 12:59:34 +0100

    Update lib.rs with documentation comments

--- npm-expansions/src/lib.rs
+++ npm-expansions/src/lib.rs
@@ -1,11 +1,39 @@
+//! # Npm Expansions
+//!
+//! A library creating a npm expansions web server based on HTTP/1.1
+
+#![warn(missing_docs)]
+#![warn(missing_doc_code_examples)]
+
+/// A series of default HTTP response methods
 pub mod default_controller;
+
+/// A static database of npm expansions and methods to search them
 pub mod expansions_model;
+
+/// A series of helper functions and modules for processing mime types
 pub mod mime_type;
+
+/// A mock of the expansions model for testing purposes
 pub mod mock_expansions_model;
+
+/// A mock of the expansions model for testing purposes
 pub mod mock_tcp_stream;
+
+/// A series of JSON HTTP response methods for searching the expansions model
 pub mod npm_controller;
+
+/// A generic error used to represent server failings
 pub mod npm_expansion_error;
+
+/// A representation of the different parts of a HTTP request
 pub mod request;
+
+/// A representation of a HTTP response
 pub mod response;
+
+/// A series of utilities to handle incoming HTTP requests and to response to them
 pub mod router;
+
+/// A module for taking a TCP stream and correctly handling its HTTP request
 pub mod stream_handler;
