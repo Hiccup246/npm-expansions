3eeea498583ce58dad0012a332f5771b5d2e5db9
James Watt <james.watt529@outlook.com>
2023-04-04 13:11:09 +0100

    Add documentation comments to mime_type.rs

--- npm-expansions/src/mime_type.rs
+++ npm-expansions/src/mime_type.rs
@@ -1,2 +1,5 @@
+/// A module containing functions to help evaluate the appropriateness of mime types based on their quality value
 pub mod matcher;
+
+/// A module allowing mime type strings to be transformed into structured formats
 pub mod parser;
