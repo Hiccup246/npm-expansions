da1892d6ba915f13a942d2c02a3871c9f9f952b6
James Watt <james.watt529@outlook.com>
2023-04-05 15:08:00 +0100

    Update search and all functions to separate expansions with two line breaks

--- nginx-reverse-proxy/pages/npm_expansions/npm_expansions.js
+++ nginx-reverse-proxy/pages/npm_expansions/npm_expansions.js
@@ -16,7 +16,7 @@ async function searchExpansions(query) {
     const searchExpansionResponse = await fetch(`/api/search?query=${query.target.value}`);
     const searchExpansionJSONResponse = await searchExpansionResponse.json();
     
-    const textareaString = searchExpansionJSONResponse.reduce((acc, expansion) => acc + (expansion + "\n"), "");
+    const textareaString = searchExpansionJSONResponse.reduce((acc, expansion) => acc + (expansion + "\n\n"), "");
     document.querySelector(".results-expansions-list").innerHTML = textareaString;
     document.querySelector(".results-expansions-list").setAttribute("rows", searchExpansionJSONResponse.length);
 }
@@ -25,7 +25,7 @@ async function loadAllExpansions() {
     const allExpansionResponse = await fetch("/api/all");
     const allExpansionJSONResponse = await allExpansionResponse.json();
 
-    const textareaString = allExpansionJSONResponse.reduce((acc, expansion) => acc + (expansion + "\n"), "");
+    const textareaString = allExpansionJSONResponse.reduce((acc, expansion) => acc + (expansion + "\n\n"), "");
     document.querySelector(".results-expansions-list").innerHTML = textareaString;
     document.querySelector(".results-expansions-list").setAttribute("rows", allExpansionJSONResponse.length);
 }
