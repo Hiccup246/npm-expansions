fb4c6aaab403a8d4dcc73f9268601020ef8ad210
James Watt <james.watt529@outlook.com>
2023-04-11 11:41:42 +0100

    Add logs to nginx-reverse-proxy main program

--- nginx-reverse-proxy/src/main.rs
+++ nginx-reverse-proxy/src/main.rs
@@ -10,30 +10,43 @@ fn main() {
         let (key, val) = arg
             .split_once('=')
             .expect("Command line arguments must be in format KEY=val");
+        
+        println!("Injecting env variable: {}", key);
+        
         env_variables.insert(key.trim().to_string(), val.to_string());
     }
 
+    println!("Injecting env variables into pages directory...");
     env_injector::inject_envs_into_drectory(
         PathBuf::from("pages"),
         PathBuf::from("injected_pages"),
         &env_variables,
     )
     .unwrap();
+    println!("Successfully injected env variables into pages directory!");
+
+    println!("Injecting env variables into static directory...");
     env_injector::inject_envs_into_drectory(
         PathBuf::from("static"),
         PathBuf::from("injected_static"),
         &env_variables,
     )
     .unwrap();
+    println!("Successfully injected env variables into pages directory!");
 
+    println!("Minifying pages directory...");
     minify::minify_drectory(
         PathBuf::from("injected_pages"),
         PathBuf::from("minified_pages"),
     )
     .unwrap();
+    println!("Successfully minified pages directory!");
+
+    println!("Minifying pages directory...");
     minify::minify_drectory(
         PathBuf::from("injected_static"),
         PathBuf::from("minified_static"),
     )
     .unwrap();
+    println!("Successfully minified static directory!");
 }
