b74d32829fb82b5eebc2f13275fac2fc2e51298d
James Watt <james.watt529@outlook.com>
2023-04-16 22:53:08 +0100

    Add draft of github_api module

--- npm-expansions/src/github_api.rs
+++ npm-expansions/src/github_api.rs
@@ -2,7 +2,7 @@ use reqwest;
 use serde_json;
 use std::collections::HashMap;
 
-struct GithubApi {
+pub struct GithubApi {
     user_agent: String,
     client: reqwest::blocking::Client,
 }
@@ -13,12 +13,16 @@ impl GithubApi {
             .user_agent(user_agent)
             .build()?;
 
-        Ok(GithubApi { user_agent: user_agent.to_string(), client })
+        Ok(GithubApi {
+            user_agent: user_agent.to_string(),
+            client,
+        })
     }
 
     pub fn repo_pr_numbers(&self, repo_url: &str) -> Result<Vec<String>, reqwest::Error> {
         let repo_prs_url = repo_url.to_owned() + "/pulls";
-        let repo_prs: Vec<serde_json::Value> = self.client
+        let repo_prs: Vec<serde_json::Value> = self
+            .client
             .get(repo_prs_url)
             .send()?
             .json::<Vec<serde_json::Value>>()?;
@@ -29,10 +33,14 @@ impl GithubApi {
             .collect())
     }
 
-    pub fn fetch_pr_raw_file_urls(&self, pr_url: &str) ->  Result<HashMap<String, String>, reqwest::Error> {
+    pub fn fetch_pr_raw_file_urls(
+        &self,
+        pr_url: &str,
+    ) -> Result<HashMap<String, String>, reqwest::Error> {
         let pr_files_url = pr_url.to_owned() + "/files";
 
-        let repo_files: Vec<serde_json::Value> = self.client
+        let repo_files: Vec<serde_json::Value> = self
+            .client
             .get(pr_files_url)
             .send()?
             .json::<Vec<serde_json::Value>>()?;
@@ -52,8 +60,8 @@ impl GithubApi {
                     None
                 }
             });
-    
-            Ok(HashMap::from_iter(name_and_raw_url))
+
+        Ok(HashMap::from_iter(name_and_raw_url))
     }
 
     pub fn fetch_pr_file(&self, raw_file_url: &str) -> Result<Vec<u8>, reqwest::Error> {
@@ -75,13 +83,11 @@ mod tests {
             let mut mock_server = mockito::Server::new();
             let repo_url = mock_server.url() + "/repos/npm/npm-expansions";
 
-
             let mock = mock_server
                 .mock("GET", "/repos/npm/npm-expansions/pulls")
                 .with_status(200)
                 .with_body("[{\"number\": 4301},{\"number\": 4302},{\"number\": 4303}]")
                 .create();
-
 
             github_api.repo_pr_numbers(repo_url.as_str()).unwrap();
 
@@ -221,7 +227,8 @@ mod tests {
             let github_api = GithubApi::new("npm-expansions").unwrap();
             let mut mock_server = mockito::Server::new();
 
-            mock_server.mock("GET", "/repos/npm/npm-expansions/pulls/4302/files")
+            mock_server
+                .mock("GET", "/repos/npm/npm-expansions/pulls/4302/files")
                 .with_status(400)
                 .create();
 
@@ -294,9 +301,7 @@ mod tests {
 
             assert_eq!(
                 files,
-                HashMap::from([
-                    ("hello-world.txt".to_string(), "url-to-file".to_string())
-                ])
+                HashMap::from([("hello-world.txt".to_string(), "url-to-file".to_string())])
             )
         }
 
@@ -316,9 +321,7 @@ mod tests {
 
             assert_eq!(
                 files,
-                HashMap::from([
-                    ("hello-world.txt".to_string(), "url-to-file".to_string())
-                ])
+                HashMap::from([("hello-world.txt".to_string(), "url-to-file".to_string())])
             )
         }
     }
@@ -331,7 +334,8 @@ mod tests {
             let github_api = GithubApi::new("npm-expansions").unwrap();
             let mut mock_server = mockito::Server::new();
 
-            mock_server.mock("GET", "/example.txt")
+            mock_server
+                .mock("GET", "/example.txt")
                 .with_status(200)
                 .with_body(b"Hello World!")
                 .create();
